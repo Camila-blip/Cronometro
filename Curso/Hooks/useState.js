1fde5425cf31a0ae9689ba1e812f7ae10b0626e2
Camila-blip <59473635+Camila-blip@users.noreply.github.com>
2022-02-06 10:03:52 -0300

    Update useState.js

--- Curso/Hooks/useState.js
+++ Curso/Hooks/useState.js
@@ -1,15 +1,18 @@
- 
 import react, { useState } from 'react';
 
 function App() {
+    // Primeiro parametro é o nome da state e o segundo e o nome que a gente vai usar para atualizar o valor da state
+    //Agora state são separadas
     const [tarefas, setTarefas] = useState([
         'Pagar a conta de luz',
         'Estudar React Hooks'
     ]);
-    const [input, setInput] = useState(null);
+    const [input, setInput] = useState('');
 
     function handleAdd() {
+        // consegue acessar os valores anteriores com ...tarefas, e após a virgula adiciona uma nova
         setTarefas([...tarefas, input])
+        setInput('');
     }
 
     return (
