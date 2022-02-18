964efa2e58394eb5329c08de4e999e0ba9f18702
Camila-blip <59473635+Camila-blip@users.noreply.github.com>
2022-02-17 22:48:04 -0300

    Add files via upload
    Projeto em andamento.

---
+++ Projetos/Sistema/pages/SignIn/index.js
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { Container,  Login, LogoArea, LogoImagem, Form, Input, Button, LinkHref, H1 } from './SignIn';
+
+
+export default function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit(e){
+    e.preventDefault();
+    alert('aqui')
+  }
+
+    return (
+      <Container>
+        <Login>
+          <LogoArea>
+            <LogoImagem />
+          </LogoArea>
+          <Form onSubmit={handleSubmit}>
+              <H1>Entrar</H1>
+              <Input type="text" placeholder='email@email.com' value={email}  onChange={ (e) => setEmail(e.target.value)}/>
+              <Input type="password" placeholder='*********' value={password} onChange={(e) => setPassword(e.target.value)}/>
+              <Button type="submit">Acessar</Button>
+          </Form>
+          <LinkHref  to="/register">Criar uma conta</LinkHref>
+        </Login>
+      </Container>
+    );
+  }
+  
