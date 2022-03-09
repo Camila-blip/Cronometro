e323b4a6e80ad252732d985a588d3f7562312567
Camila-blip <59473635+Camila-blip@users.noreply.github.com>
2022-03-08 22:45:56 -0300

    Update auth.js
    Criei a parte de cadastro, login e logout.

--- Projetos/Sistema/contexts/auth.js
+++ Projetos/Sistema/contexts/auth.js
@@ -10,7 +10,7 @@ function AuthProvider({ children }){
     // !!user as duas exclamação converte para bool se caso tenha algo true
     useEffect(()=>{
         function loadStorage(){
-            const storageUser = localStorage.getItem('SitemaUser');
+            const storageUser = localStorage.getItem('SistemaUser');
             if(storageUser){
                 setUser(JSON.parse(storageUser));
                 setLoading(false);
@@ -19,8 +19,77 @@ function AuthProvider({ children }){
         }
         loadStorage();
     }, [])
+
+    //login
+    async function signIn(email,password){
+        setLoadingAuth(true);
+        await firebase.auth().signInWithEmailAndPassword(email,password)
+        .then(async (value)=>{
+            let uid = value.user.uid;
+            const userProfile = await firebase.firestore().collection('users')
+            .doc(uid).get()
+            let data = {
+                uid: uid,
+                nome: userProfile.data().nome,
+                avatarUrl: userProfile.data().avatarUrl,
+                email: value.user.email
+            };
+
+            setUser(data);
+            storageUser(data);
+            setLoadingAuth(false);
+
+        })
+        .catch((error)=>{
+            console.log(error)
+            alert('Erro no login')
+            setLoadingAuth(false);
+        })
+
+    }
+    //cadastro
+    async function signUp(email, password, nome){
+        setLoadingAuth(true);      
+        await firebase.auth().createUserWithEmailAndPassword(email,password)
+        .then( async (value)=>{
+            let uid = value.user.uid;
+            await firebase.firestore().collection('users')
+            .doc(uid).set({
+                nome: nome,
+                avatarUrl: null
+            })
+            .then(()=>{
+                let data = {
+                    uid: uid,
+                    nome: nome,
+                    email: value.user.email,
+                    avatarUrl: null
+                };
+                setUser(data);
+                storageUser(data);
+                setLoadingAuth(false);
+            })
+        })
+        .catch((error)=>{
+            console.log(error)
+            alert('Erro no cadastro')
+            setLoadingAuth(false);
+        })
+    }
+
+    function storageUser(data){
+        localStorage.setItem('SistemaUser', JSON.stringify(data));
+    }
+
+    //logout
+    async function signOut(){
+        await firebase.auth().signOut();
+        localStorage.removeItem('SistemaUser');
+        setUser(null);
+    }
+
     return(
-        <AuthContent.Provider value={{signed: !!user, user, loading }}>
+        <AuthContent.Provider value={{signed: !!user, user, loading, signUp, signOut, signIn, loadingAuth }}>
             {children}
         </AuthContent.Provider>
     )
