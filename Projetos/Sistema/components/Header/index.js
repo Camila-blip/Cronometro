cea847ecd947faee712446901b0f129861e100c0
Camila-blip <59473635+Camila-blip@users.noreply.github.com>
2022-03-10 00:38:01 -0300

    Update index.js

--- Projetos/Sistema/components/Header/index.js
+++ Projetos/Sistema/components/Header/index.js
@@ -1,9 +1,8 @@
 import { useContext } from 'react';
 import { AuthContent } from '../../contexts/auth';
 import avatar from '../../Assets/avatar.png';
-import { Link } from 'react-router-dom';
 import { FiHome, FiUser, FiSettings } from "react-icons/fi";
-import {Sidebar,Avatar} from './headerStyle';
+import { Sidebar, Avatar, Img, LinkHref } from './headerStyle';
 
 export default function Header(){
     const { user } = useContext(AuthContent);
@@ -11,20 +10,20 @@ export default function Header(){
     return(
         <Sidebar>
             <Avatar>
-                <img src={user.avatarUrl === null ? avatar : user.avatarUrl } alt="Foto avatar" />
+                <Img src={user.avatarUrl === null ? avatar : user.avatarUrl } alt="Foto avatar" />
             </Avatar>
-            <Link to="/dashboard">
-                <FiHome color="#000" size={23} />
+            <LinkHref to="/dashboard">
+                <FiHome  size={23} />
                 Chamados
-            </Link>
-            <Link to="/dashboard">
-                <FiUser color="#000" size={23} />
+            </LinkHref>
+            <LinkHref to="/dashboard">
+                <FiUser  size={23} />
                 Clientes
-            </Link>
-            <Link to="/dashboard">
-                <FiSettings color="#000" size={23} />
+            </LinkHref>
+            <LinkHref to="/dashboard">
+                <FiSettings  size={23} />
                 Configurações
-            </Link>
+            </LinkHref>
         </Sidebar>
     )
 }
