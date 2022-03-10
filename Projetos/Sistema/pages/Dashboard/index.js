5af3f0bcde19e9dd821170ffdc8ffb4855608732
Camila-blip <59473635+Camila-blip@users.noreply.github.com>
2022-03-09 23:56:32 -0300

    Update index.js

--- Projetos/Sistema/pages/Dashboard/index.js
+++ Projetos/Sistema/pages/Dashboard/index.js
@@ -1,3 +1,15 @@
+import { useContext } from "react";
+import { AuthContent } from "../../contexts/auth";
+import Header from "../../components/Header";
+
 export default function Dashboard(){
-    
+    const { signOut } = useContext( AuthContent );
+
+    return(
+        <div>
+            <Header/>
+           
+            <button onClick={()=> signOut()}>Fazer logout</button>
+        </div>
+    )
 }
