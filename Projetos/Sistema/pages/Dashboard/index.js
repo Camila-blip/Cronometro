bc00c50e80bc73aea73e0553335c977c558f2b46
Camila-blip <59473635+Camila-blip@users.noreply.github.com>
2022-03-12 00:25:11 -0300

    Update index.js

--- Projetos/Sistema/pages/Dashboard/index.js
+++ Projetos/Sistema/pages/Dashboard/index.js
@@ -1,14 +1,10 @@
-import { useContext } from "react";
-import { AuthContent } from "../../contexts/auth";
 import Header from "../../components/Header";
 
 export default function Dashboard(){
-    const { signOut } = useContext( AuthContent );
-
     return(
         <div>
             <Header/>
-            <button onClick={()=> signOut()}>Fazer logout</button>
+           <h1>Dashboard</h1>
         </div>
     )
 }
