605e1639aac025474c8bc3fe65213c972beda718
Camila-blip <59473635+Camila-blip@users.noreply.github.com>
2022-03-13 23:45:19 -0300

    Update index.js

--- Projetos/Sistema/routes/index.js
+++ Projetos/Sistema/routes/index.js
@@ -4,6 +4,7 @@ import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import Dashboard from '../pages/Dashboard';
 import Profile from '../pages/Profile';
+import Customers from '../pages/Customers';
 
 export default function Rotas(){
     return(
@@ -11,6 +12,7 @@ export default function Rotas(){
             <Route exact path="/" component={SignIn}/>
             <Route exact path="/register" component={SignUp}/>
 
+            <Route exact path="/customers" component={Customers} isPrivate/>
             <Route exact path="/dashboard" component={Dashboard} isPrivate/>
             <Route exact path="/profile" component={Profile} isPrivate/>
         </Switch>
